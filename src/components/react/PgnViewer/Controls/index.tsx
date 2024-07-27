efb33704eab04a52997123fdf671837533cccca6
David Gilhooley <djgilhooley@gmail.com>
2024-07-27 16:25:22 -0400

    feat: Add titles to buttons

--- src/components/react/PgnViewer/Controls/index.tsx
+++ src/components/react/PgnViewer/Controls/index.tsx
@@ -13,21 +13,21 @@ export const Controls = (props: ControlActions) => {
 	return (
 		<div>
 			<div className="button-section">
-				<button onClick={() => props.onBackButtonClick()}>
+				<button title="Back" onClick={() => props.onBackButtonClick()}>
 					<ArrowLeft />
 				</button>
-				<button onClick={() => props.onForwardButtonClick()}>
+				<button title="Forward" onClick={() => props.onForwardButtonClick()}>
 					<ArrowRight />
 				</button>
-				<button onClick={() => props.onSaveButtonClick()}>
+				<button title="Save" onClick={() => props.onSaveButtonClick()}>
 					<Save strokeWidth={'1px'} />
 				</button>
 			</div>
 			<div className="button-section">
-				<button onClick={() => props.onCopyButtonClick()}>
+				<button title="Copy FEN" onClick={() => props.onCopyButtonClick()}>
 					<Copy strokeWidth={'1px'} />
 				</button>
-				<button onClick={() => props.onUndoButtonClick()}>
+				<button title="Undo" onClick={() => props.onUndoButtonClick()}>
 					<Undo2 />
 				</button>
 			</div>
